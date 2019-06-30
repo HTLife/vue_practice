56219a7e95cdd5cc52037e9a0b46e67a275944e4
Jacky Liu <jjkka132@gmail.com>
2019-06-30 20:10:59 +0900

    pcd

--- myproject3_pcd/src/components/Home.vue
+++ myproject3_pcd/src/components/Home.vue
@@ -4,8 +4,6 @@
 
 <script>
 
-// import * as THREE from 'three/build/three.module.js'
-
 import {
   Color,
   PerspectiveCamera,
@@ -24,64 +22,85 @@ export default {
   name: 'ThreeTest',
   data () {
     return {
-      cube: null,
       renderer: null,
       scene: null,
-      camera: null
+      camera: null,
+      container: null,
+      controls: null,
+      stats: null
     }
   },
   methods: {
 
     init: function () {
-      // var stats
-      var stats
-      var camera, controls, scene, renderer
+      this.container = document.getElementById('pointcloud_container')
 
-      let container = document.getElementById('pointcloud_container')
-
-      scene = new Scene()
-      scene.background = new Color(0x000000)
-      camera = new PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.01, 40)
-      camera.position.x = 0.4
-      camera.position.z = -2
-      camera.up.set(0, 0, 1)
-      scene.add(camera)
-      renderer = new WebGLRenderer({ antialias: true })
-      renderer.setPixelRatio(window.devicePixelRatio)
-      renderer.setSize(window.innerWidth, window.innerHeight)
-      document.body.appendChild(renderer.domElement)
+      this.scene = new Scene()
+      this.scene.background = new Color(0x000000)
+      this.camera = new PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.01, 40)
+      this.camera.position.x = 0.4
+      this.camera.position.z = -2
+      this.camera.up.set(0, 0, 1)
+      this.scene.add(this.camera)
+      this.renderer = new WebGLRenderer({ antialias: true })
+      this.renderer.setPixelRatio(window.devicePixelRatio)
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
+      document.body.appendChild(this.renderer.domElement)
       var loader = new PCDLoader()
       loader.load('https://raw.githubusercontent.com/mrdoob/three.js/4b18dbe78bec6067cd98e66539efe1b157f5635f/examples/models/pcd/ascii/simple.pcd', function (points) {
-        scene.add(points)
+        this.scene.add(points)
         var center = points.geometry.boundingSphere.center
-        controls.target.set(center.x, center.y, center.z)
-        controls.update()
+        this.controls.target.set(center.x, center.y, center.z)
+        this.controls.update()
       })
-      // container = document.createElement('div')
-      // document.body.appendChild(container)
-      container.appendChild(renderer.domElement)
-      controls = new TrackballControls(camera, renderer.domElement)
-      controls.rotateSpeed = 2.0
-      controls.zoomSpeed = 0.3
-      controls.panSpeed = 0.2
-      controls.noZoom = false
-      controls.noPan = false
-      controls.staticMoving = true
-      controls.dynamicDampingFactor = 0.3
-      controls.minDistance = 0.3
-      controls.maxDistance = 0.3 * 100
-      stats = new Stats()
-      container.appendChild(stats.dom)
-      // window.addEventListener('resize', onWindowResize, false)
-      // window.addEventListener('keypress', keyboard)
 
-      // container.appendChild(renderer.domElement)
+      this.container.appendChild(this.renderer.domElement)
+
+      this.controls = new TrackballControls(this.camera, this.renderer.domElement)
+      this.controls.rotateSpeed = 2.0
+      this.controls.zoomSpeed = 0.3
+      this.controls.panSpeed = 0.2
+      this.controls.noZoom = false
+      this.controls.noPan = false
+      this.controls.staticMoving = true
+      this.controls.dynamicDampingFactor = 0.3
+      this.controls.minDistance = 0.3
+      this.controls.maxDistance = 0.3 * 100
+
+      this.stats = new Stats()
+      this.container.appendChild(this.stats.dom)
+
+      window.addEventListener('resize', this.onWindowResize, false)
+      window.addEventListener('keypress', this.keyboard)
+    },
+    onWindowResize: function () {
+      this.camera.aspect = window.innerWidth / window.innerHeight
+      this.camera.updateProjectionMatrix()
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
+      this.controls.handleResize()
+    },
+    keyboard: function (ev) {
+      var points = this.scene.getObjectByName('simple.pcd')
+      switch (ev.key || String.fromCharCode(ev.keyCode || ev.charCode)) {
+        case '+':
+          points.material.size *= 1.2
+          points.material.needsUpdate = true
+          break
+        case '-':
+          points.material.size /= 1.2
+          points.material.needsUpdate = true
+          break
+        case 'c':
+          points.material.color.setHex(Math.random() * 0xffffff)
+          points.material.needsUpdate = true
+          break
+      }
     },
     animate: function () {
-      // requestAnimationFrame(animate)
-      // controls.update()
-      // renderer.render(scene, camera)
-      // stats.update()
+      requestAnimationFrame(this.animate)
+      this.controls.update()
+      this.renderer.render(this.scene, this.camera)
+      this.stats.update()
     }
   },
   mounted () {
